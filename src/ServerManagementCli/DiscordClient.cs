56c61c21d3bf2e1cac323057278ba954f7f5840d
Harrison Lambeth <harrison@nau.edu>
2021-08-27 17:46:48 -0700

    Add new project for managing server configurations

---
+++ src/ServerManagementCli/DiscordClient.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerManagementCli
+{
+    public class DiscordClient
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly string _apiKey;
+
+        public DiscordClient(string apiKey)
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+
+            });
+            _client.Log += OnLog;
+            //_client.Ready += OnReady;
+            //_client.MessageReceived += OnMessageReceived;
+
+            _apiKey = apiKey;
+        }
+
+        public async Task StartAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, _apiKey);
+            await _client.StartAsync();
+        }
+
+        private Task OnLog(LogMessage arg)
+        {
+            Console.WriteLine(arg.Message);
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/ServerManagementCli/Program.cs
@@ -0,0 +1,36 @@
+﻿using DankDitties;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerManagementCli
+{
+    class Program
+    {
+        public static readonly string ServerConfigFileLocation = Utils.GetEnv("SERVER_CONFIG_FILE") ?? throw new Exception("missing server config file location");
+        public static readonly ulong DiscordServerId = ulong.Parse(Utils.GetEnv("SERVER_ID", "493935564832374795"));
+        public static readonly string? DiscordApiKeyOverride = Utils.GetEnv("DISCORD_API_KEY");
+
+        public static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            var secrets = JsonConvert.DeserializeObject<Secrets>(File.ReadAllText("secrets.json"));
+            if (secrets == null)
+                throw new Exception("secrets object was null");
+            secrets.DiscordApiKey = DiscordApiKeyOverride ?? secrets.DiscordApiKey;
+
+            if (secrets.DiscordApiKey == null)
+                throw new NullReferenceException("DiscordApiKey must be provided.");
+            var client = new DiscordClient(secrets.DiscordApiKey);
+            await client.StartAsync();
+
+            Console.CancelKeyPress += (o, e) => cts.Cancel();
+            AppDomain.CurrentDomain.ProcessExit += (s, ev) => cts.Cancel();
+
+            await cts.Token.WhenCancelled();
+        }
+    }
+}
