bf4b32cf29e26050856d7657da1f23742f4b2569
Harrison Lambeth <harrison@nau.edu>
2021-08-27 22:03:07 -0700

    Basic configuration loading

--- src/ServerManagementCli/DiscordClient.cs
+++ src/ServerManagementCli/DiscordClient.cs
@@ -20,16 +20,71 @@ namespace ServerManagementCli
 
             });
             _client.Log += OnLog;
-            //_client.Ready += OnReady;
+            _client.Ready += OnReadyAsync;
             //_client.MessageReceived += OnMessageReceived;
 
             _apiKey = apiKey;
+        }
+
+        private Task OnReadyAsync()
+        {
+            var guild = _client.Guilds.First(g => g.Id == Program.DiscordServerId);
+
+            var existingConfiguration = new ServerConfiguration();
+            var nextId = 0;
+
+            foreach (var role in guild.Roles)
+            {
+                if (role == null)
+                    continue;
+
+                existingConfiguration.Roles.Add(role.Id.ToString(), new ServerConfigurationRole()
+                {
+                    Id = role.Id,
+                    Name = role.Name,
+                    Position = role.Position,
+                    Color = role.Color.ToString().TrimStart('#'),
+                    Membership = role.Members.Select(m => m.Username).ToList(),
+                }); 
+            }
+
+            var channelMappings = new Dictionary<ulong, ulong>();
+            foreach (var category in guild.CategoryChannels)
+            {
+                if (category == null)
+                    continue;
+
+                var categoryId = category.Id.ToString();
+                existingConfiguration.Categories.Add(categoryId, new ServerConfigurationCategory()
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    Position = category.Position,
+                });
+
+                foreach (var channel in category.Channels)
+                {
+                    if (channel == null)
+                        continue;
+
+                    existingConfiguration.Channels.Add(channel.Id.ToString(), new ServerConfigurationChannel()
+                    {
+                        Id = channel.Id,
+                        Name = channel.Name,
+                        Category = categoryId,
+                        Position = channel.Position,
+                    });
+                }
+            }
+
+            return Task.FromResult(0);
         }
 
         public async Task StartAsync()
         {
             await _client.LoginAsync(TokenType.Bot, _apiKey);
             await _client.StartAsync();
+
         }
 
         private Task OnLog(LogMessage arg)
---
+++ src/ServerManagementCli/ServerConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerManagementCli
+{
+    public class ServerConfiguration
+    {
+        public Dictionary<string, ServerConfigurationRole> Roles { get; set; } = new Dictionary<string, ServerConfigurationRole>();
+        public Dictionary<string, ServerConfigurationCategory> Categories { get; set; } = new Dictionary<string, ServerConfigurationCategory>();
+        public Dictionary<string, ServerConfigurationChannel> Channels { get; set; } = new Dictionary<string, ServerConfigurationChannel>();
+    }
+
+    public class ServerConfigurationRole
+    {
+        public ulong Id { get; set; }
+        public string? Name { get; set; }
+        public int Position { get; set; }
+        public string? Color { get; set; }
+        public PermissionSet Permissions { get; set; } = new PermissionSet();
+        public List<string> Membership { get; set; } = new List<string>();
+    }
+
+    public class ServerConfigurationCategory
+    {
+        public ulong Id { get; set; }
+        public string? Name { get; set; }
+        public int Position { get; set; }
+
+        public Dictionary<string, PermissionSet> Permissions { get; set; } = new Dictionary<string, PermissionSet>();
+    }
+
+    public class ServerConfigurationChannel
+    {
+        public ulong Id { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public int Position { get; set; }
+
+        public Dictionary<string, PermissionSet> Permissions { get; set; } = new Dictionary<string, PermissionSet>();
+    }
+
+    public class PermissionSet
+    {
+        public bool View { get; set; }
+        public bool Connect { get; set; }
+    }
+}
