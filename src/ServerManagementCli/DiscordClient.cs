2736f9f96e7b536072fe60a78c78d96d93096e4b
Harrison Lambeth <harrison@nau.edu>
2021-08-28 01:15:17 -0700

    Yaml reading/writing & comparison

--- src/ServerManagementCli/DiscordClient.cs
+++ src/ServerManagementCli/DiscordClient.cs
@@ -1,10 +1,16 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using KellermanSoftware.CompareNetObjects;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace ServerManagementCli
 {
@@ -17,7 +23,7 @@ namespace ServerManagementCli
         {
             _client = new DiscordSocketClient(new DiscordSocketConfig()
             {
-
+                AlwaysDownloadUsers = true,
             });
             _client.Log += OnLog;
             _client.Ready += OnReadyAsync;
@@ -26,58 +32,194 @@ namespace ServerManagementCli
             _apiKey = apiKey;
         }
 
-        private Task OnReadyAsync()
+        private async Task OnReadyAsync()
         {
-            var guild = _client.Guilds.First(g => g.Id == Program.DiscordServerId);
-
-            var existingConfiguration = new ServerConfiguration();
-            var nextId = 0;
-
-            foreach (var role in guild.Roles)
+            try
             {
-                if (role == null)
-                    continue;
+                var guild = _client.Guilds.First(g => g.Id == Program.DiscordServerId);
+                await guild.DownloadUsersAsync();
 
-                existingConfiguration.Roles.Add(role.Id.ToString(), new ServerConfigurationRole()
+                var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(UnderscoredNamingConvention.Instance)
+                    .Build();
+                var serializer = new SerializerBuilder()
+                    .WithNamingConvention(UnderscoredNamingConvention.Instance)
+                    .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitDefaults | DefaultValuesHandling.OmitEmptyCollections | DefaultValuesHandling.OmitNull)
+                    .Build();
+
+                var expectedConfiguration = deserializer.Deserialize<ServerConfiguration>(File.ReadAllText(Program.ServerConfigFileLocation));
+
+                var existingConfiguration = new ServerConfiguration();
+
+                foreach (var role in guild.Roles)
                 {
-                    Id = role.Id,
-                    Name = role.Name,
-                    Position = role.Position,
-                    Color = role.Color.ToString().TrimStart('#'),
-                    Membership = role.Members.Select(m => m.Username).ToList(),
-                }); 
-            }
-
-            var channelMappings = new Dictionary<ulong, ulong>();
-            foreach (var category in guild.CategoryChannels)
-            {
-                if (category == null)
-                    continue;
-
-                var categoryId = category.Id.ToString();
-                existingConfiguration.Categories.Add(categoryId, new ServerConfigurationCategory()
-                {
-                    Id = category.Id,
-                    Name = category.Name,
-                    Position = category.Position,
-                });
-
-                foreach (var channel in category.Channels)
-                {
-                    if (channel == null)
+                    if (role == null)
                         continue;
 
-                    existingConfiguration.Channels.Add(channel.Id.ToString(), new ServerConfigurationChannel()
+                    var matchingRole = expectedConfiguration.Roles.FirstOrDefault(kvp => kvp.Value.Id == role.Id);
+                    var friendlyId = matchingRole.Key ?? _generateFriendlyName(role.Name);
+                    var needsMembership = matchingRole.Value?.HasManagedMembership == true;
+                    existingConfiguration.Roles.Add(friendlyId, new ServerConfigurationRole()
                     {
-                        Id = channel.Id,
-                        Name = channel.Name,
-                        Category = categoryId,
-                        Position = channel.Position,
+                        Id = role.Id,
+                        Name = role.Name,
+                        Position = role.Position,
+                        Color = role.Color.ToString().TrimStart('#'),
+                        HasManagedMembership = needsMembership,
+                        Membership = needsMembership ? role.Members.Select(_getGoodName).ToList() : new List<string>(),
+                        Permissions = _mapPermissions(role.Permissions),
                     });
                 }
+
+                var channelMappings = new Dictionary<ulong, ulong>();
+                foreach (var category in guild.CategoryChannels)
+                {
+                    if (category == null)
+                        continue;
+
+                    var matchingCategory = expectedConfiguration.Categories.FirstOrDefault(kvp => kvp.Value.Id == category.Id);
+                    var categoryFriendlyId = matchingCategory.Key ?? _generateFriendlyName(category.Name);
+                    existingConfiguration.Categories.Add(categoryFriendlyId, new ServerConfigurationCategory()
+                    {
+                        Id = category.Id,
+                        Name = category.Name,
+                        Position = category.Position,
+                        RolePermissions = category.PermissionOverwrites
+                                        .Where(p => p.TargetType == PermissionTarget.Role)
+                                        .ToDictionary(
+                                            p => existingConfiguration.Roles.FirstOrDefault(role => role.Value.Id == p.TargetId).Key,
+                                            p => _mapPermissions(p.Permissions)
+                                        ),
+                        UserPermissions = category.PermissionOverwrites
+                                        .Where(p => p.TargetType == PermissionTarget.User)
+                                        .ToDictionary(
+                                            p => _getGoodName(guild.Users.FirstOrDefault(user => user.Id == p.TargetId)),
+                                            p => _mapPermissions(p.Permissions)
+                                        ),
+                    });
+
+                    foreach (var channel in category.Channels)
+                    {
+                        if (channel == null)
+                            continue;
+
+                        var matchingChannel = matchingCategory.Value?.Channels.FirstOrDefault(kvp => kvp.Value.Id == channel.Id);
+                        var channelFriendlyId = matchingChannel?.Key ?? _generateFriendlyName(channel.Name + (channel is IVoiceChannel ? "_voice" : ""));
+                        existingConfiguration.Categories[categoryFriendlyId].Channels.Add(channelFriendlyId, new ServerConfigurationChannel()
+                        {
+                            Id = channel.Id,
+                            Name = channel.Name,
+                            Category = categoryFriendlyId,
+                            Position = channel.Position,
+                            RolePermissions = channel.PermissionOverwrites
+                                        .Where(p => p.TargetType == PermissionTarget.Role)
+                                        .Select(p => new KeyValuePair<string, PermissionSet?>(
+                                                existingConfiguration.Roles.FirstOrDefault(role => role.Value.Id == p.TargetId).Key,
+                                                _mapPermissions(p.Permissions, category.PermissionOverwrites.FirstOrDefault(po => po.TargetId == p.TargetId && po.TargetType == p.TargetType).Permissions)
+                                            ))
+                                        .Where(kvp => kvp.Value != null)
+                                        .ToDictionary(kvp => kvp.Key, kvp => kvp.Value!),
+                            UserPermissions = channel.PermissionOverwrites
+                                        .Where(p => p.TargetType == PermissionTarget.User)
+                                        .Select(p => new KeyValuePair<string, PermissionSet?>(
+                                                _getGoodName(guild.Users.FirstOrDefault(user => user.Id == p.TargetId)),
+                                                _mapPermissions(p.Permissions, category.PermissionOverwrites.FirstOrDefault(po => po.TargetId == p.TargetId && po.TargetType == p.TargetType).Permissions)
+                                            ))
+                                        .Where(kvp => kvp.Value != null)
+                                        .ToDictionary(kvp => kvp.Key, kvp => kvp.Value!),
+                        });
+                    }
+                }
+
+                var cl = new CompareLogic(new ComparisonConfig()
+                {
+                    MaxDifferences = int.MaxValue,
+                });
+                var comparison = cl.Compare(expectedConfiguration, existingConfiguration);
+
+                var yaml = serializer.Serialize(existingConfiguration);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        private static string _getGoodName(IUser? user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+            return user.Username + "#" + user.Discriminator;
+        }
+
+        private static PermissionSet _mapPermissions(GuildPermissions permissions)
+        {
+            var result = new PermissionSet();
+            foreach (var prop in result.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                var srcProp = permissions.GetType().GetProperty(prop.Name);
+                if (srcProp == null)
+                    continue;
+                var v = srcProp.GetValue(permissions);
+                prop.SetValue(result, v);
+            }
+            return result;
+        }
+        private static PermissionSet _mapPermissions(OverwritePermissions permissions)
+        {
+            var result = new PermissionSet();
+            foreach (var prop in result.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                var srcProp = permissions.GetType().GetProperty(prop.Name);
+                if (srcProp == null)
+                    continue;
+                var v = _mapPermValue((PermValue)srcProp.GetValue(permissions)!);
+                prop.SetValue(result, v);
+            }
+            return result;
+        }
+        private static PermissionSet? _mapPermissions(OverwritePermissions permissions, OverwritePermissions parent)
+        {
+            var result = new PermissionSet();
+            var isAllNull = true;
+            foreach (var prop in result.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                var srcProp = permissions.GetType().GetProperty(prop.Name);
+                if (srcProp == null)
+                    continue;
+                var v = _mapPermValue((PermValue)srcProp.GetValue(permissions)!);
+                var pv = _mapPermValue((PermValue)srcProp.GetValue(parent)!);
+                var newValue = v == pv ? null : v;
+                prop.SetValue(result, newValue);
+
+                if (newValue != null)
+                    isAllNull = false;
             }
 
-            return Task.FromResult(0);
+            if (isAllNull)
+                return null;
+
+            return result;
+        }
+
+        private static bool? _mapPermValue(PermValue permValue)
+        {
+            return permValue switch
+            {
+                PermValue.Allow => true,
+                PermValue.Deny => false,
+                _ => null,
+            };
+        }
+
+        private static string _generateFriendlyName(string name)
+        {
+            var underscoreRegex = new Regex(@"[\-\s]");
+            var garbageRegex = new Regex(@"[^\w_]");
+            return garbageRegex.Replace(
+                underscoreRegex.Replace(name.ToLower(), "_"),
+                ""
+            );
         }
 
         public async Task StartAsync()
--- src/ServerManagementCli/ServerConfiguration.cs
+++ src/ServerManagementCli/ServerConfiguration.cs
@@ -10,7 +10,6 @@ namespace ServerManagementCli
     {
         public Dictionary<string, ServerConfigurationRole> Roles { get; set; } = new Dictionary<string, ServerConfigurationRole>();
         public Dictionary<string, ServerConfigurationCategory> Categories { get; set; } = new Dictionary<string, ServerConfigurationCategory>();
-        public Dictionary<string, ServerConfigurationChannel> Channels { get; set; } = new Dictionary<string, ServerConfigurationChannel>();
     }
 
     public class ServerConfigurationRole
@@ -19,6 +18,7 @@ namespace ServerManagementCli
         public string? Name { get; set; }
         public int Position { get; set; }
         public string? Color { get; set; }
+        public bool HasManagedMembership { get; set; }
         public PermissionSet Permissions { get; set; } = new PermissionSet();
         public List<string> Membership { get; set; } = new List<string>();
     }
@@ -29,7 +29,9 @@ namespace ServerManagementCli
         public string? Name { get; set; }
         public int Position { get; set; }
 
-        public Dictionary<string, PermissionSet> Permissions { get; set; } = new Dictionary<string, PermissionSet>();
+        public Dictionary<string, PermissionSet> RolePermissions { get; set; } = new Dictionary<string, PermissionSet>();
+        public Dictionary<string, PermissionSet> UserPermissions { get; set; } = new Dictionary<string, PermissionSet>();
+        public Dictionary<string, ServerConfigurationChannel> Channels { get; set; } = new Dictionary<string, ServerConfigurationChannel>();
     }
 
     public class ServerConfigurationChannel
@@ -38,13 +40,45 @@ namespace ServerManagementCli
         public string? Name { get; set; }
         public string? Category { get; set; }
         public int Position { get; set; }
+        public bool SyncPermissions { get; set; }
 
-        public Dictionary<string, PermissionSet> Permissions { get; set; } = new Dictionary<string, PermissionSet>();
+        public Dictionary<string, PermissionSet> RolePermissions { get; set; } = new Dictionary<string, PermissionSet>();
+        public Dictionary<string, PermissionSet> UserPermissions { get; set; } = new Dictionary<string, PermissionSet>();
     }
 
     public class PermissionSet
     {
-        public bool View { get; set; }
-        public bool Connect { get; set; }
+        public bool? AttachFiles { get; set; }
+        public bool? ReadMessageHistory { get; set; }
+        public bool? MentionEveryone { get; set; }
+        public bool? UseExternalEmojis { get; set; }
+        public bool? Connect { get; set; }
+        public bool? Speak { get; set; }
+        public bool? SendMessages { get; set; }
+        public bool? EmbedLinks { get; set; }
+        public bool? DeafenMembers { get; set; }
+        public bool? MoveMembers { get; set; }
+        public bool? UseVAD { get; set; }
+        public bool? PrioritySpeaker { get; set; }
+        public bool? Stream { get; set; }
+        public bool? MuteMembers { get; set; }
+        public bool? ManageMessages { get; set; }
+        public bool? ManageWebhooks { get; set; }
+        public bool? ManageRoles { get; set; }
+        public bool? ViewChannel { get; set; }
+        public bool? AddReactions { get; set; }
+        public bool? ManageChannel { get; set; }
+        public bool? CreateInstantInvite { get; set; }
+        public bool? SendTTSMessages { get; set; }
+
+        public bool? ChangeNickname { get; set; }
+        public bool? ManageNicknames { get; set; }
+        public bool? BanMembers { get; set; }
+        public bool? Administrator { get; set; }
+        public bool? ManageChannels { get; set; }
+        public bool? KickMembers { get; set; }
+        public bool? ViewAuditLog { get; set; }
+        public bool? ManageGuild { get; set; }
+        public bool? ManageEmojis { get; set; }
     }
 }
